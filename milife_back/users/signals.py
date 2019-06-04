f63fcdf72213cb08067bf5fabf948ce32b65b8ee
Anuvrat Parashar <bhanuvrat@gmail.com>
2019-06-04 14:14:38 +0530

    feature: populate checkin user field on user update

--- milife_back/users/signals.py
+++ milife_back/users/signals.py
@@ -1,10 +1,15 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
 from . import models
 from . import services
 
+from milife_back.fitness.models import Checkin
 
 @receiver(pre_save, sender=models.User)
 def create_accuniq_id(sender, instance, **kwargs):
     instance.accuniq_id = services.create_accuniq_id(instance.first_name, instance.last_name)
+
+@receiver(post_save, sender=models.User)
+def update_checkin_objects(sender, instance, created, **kwargs):
+    Checkin.objects.filter(user=None, accuniq_id=instance.accuniq_id).update(user=instance)
